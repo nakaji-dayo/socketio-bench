bc9392314529bf0a3565e79c1ddebee8542aee52
Daishi Nakajima <nakaji.dayo@gmail.com>
2017-06-11 15:55:39 +0900

    test

--- api/client.js
+++ api/client.js
@@ -2,37 +2,44 @@ const room = process.argv[2]
 const pNum = process.argv[3]
 const host = process.argv[4]
 
-var socket = require('socket.io-client')(host);
-
-const _ = require('lodash');
-
-
-console.log(`perf ${pNum}`)
-console.time(`perf ${pNum}`)
-
-socket.on('connect', function(){
-  socket.emit('my other event', { my: 'data' });
-  socket.emit('join', {id: room})
-  _.range(1000).forEach(x => {
-    setTimeout( () => {
-      socket.emit('msg', {body: `hello ${x}`, room});
-    }, x * 10);
+const f = (anum) => {
+  var socket = require('socket.io-client')(host, {
+    transports: ['websocket']
   });
-});
 
-socket.on('event', function(data){
-});
+  const _ = require('lodash');
 
-socket.on('news', function(data){
-});
 
-socket.on('msg', function(data){
-  if (data.body == 'HELLO 999') {
-    console.timeEnd(`perf ${pNum}`)
-    socket.close();
-    process.exit();
-  }
-});
+  console.log(`perf ${pNum} ${anum}`)
+  console.time(`perf ${pNum} ${anum}`)
 
-socket.on('disconnect', function(){
-});
+  socket.on('connect', function(){
+    console.log('on connect');
+    socket.emit('my other event', { my: 'data' });
+    socket.emit('join', {id: room + '-' + anum})
+    _.range(10).forEach(x => {
+      setTimeout( () => {
+        socket.emit('msg', {body: `hello ${x} ${anum}`, room: (room + '-' + anum)});
+      }, x * 1000);
+    });
+  });
+
+  socket.on('event', function(data){
+  });
+
+  socket.on('news', function(data){
+  });
+
+  socket.on('msg', function(data){
+    if (data.body == `HELLO 9 ${anum}`) {
+      console.timeEnd(`perf ${pNum} ${anum}`)
+      socket.close();
+    }
+  });
+
+  socket.on('disconnect', function(){
+  });
+}
+
+const _ = require('lodash')
+_.range(1250).map(f)
--- api/wb.js
+++ api/wb.js
@@ -1,19 +1,12 @@
-const mode = process.argv[2]
+const hostname = process.argv[2]
 var cp = require('child_process');
 const _ = require('lodash');
-
+console.log(hostname);
 
 var getHost = x => {
-  if (mode == 'p') {
-    // return `http://localhost:${x < 16 ? 8080 : 8081}`; //p32, 13947.226ms
-    // return `http://localhost:${8080 + Math.floor(x / 8)}`; // p32, 15178.991ms
-    return `http://localhost:${8080 + Math.floor(x / 2)}`; // p8 12746.937ms
-  } else {
-    return 'http://localhost:8080'; //p32 84503.732ms
-    //p8 14202.456ms
-  }
+  return hostname; //p32 84503.732ms
 }
 
 _.range(8).map(x =>
-                cp.fork(__dirname + '/client.js', ['test', x, getHost(x)])
+               cp.fork(__dirname + '/client.js', ['test', x, getHost(x)])
               );
